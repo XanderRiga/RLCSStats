ead041b8139cfcad7e136ae8b419c95a15f2272b
Xander <xanderriga@live.com>
2020-03-02 19:44:52 -0800

    Parse the NA standings page
    This is the most garbage code ever, but when you are parsing raw HTML
    like this...what can you do? Next up is a bunch of cleanup and making
    sure I can actually do it from the API and not just the raw json here.
    Then I will need to make sure I don't hit the API too much, because they
    already seem mad that I am hitting them so much.

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -1,25 +1,28 @@
 import * as React from 'react';
 import { BottomNavigation, Text } from 'react-native-paper';
 import StandingsList from "./StandingsList";
+import { retrieveNaStandings } from "../lib/RetrieveStandings"
 
 const StandingsRoute = () => <StandingsList rows={rows}/>;
 const MatchesRoute = () => <Text>Matches</Text>;
 const AboutRoute = () => <Text>About the creator Liquipedia RL is the data source</Text>;
 
-const rows = [
-    {
-        name: 'G2 Esports',
-        matchRecord: '4-0',
-        gameRecord: '12-3',
-        plusMinus: '+9'
-    },
-    {
-        name: 'NRG Esports',
-        matchRecord: '4-1',
-        gameRecord: '14-7',
-        plusMinus: '+7'
-    },
-];
+    const rows = retrieveNaStandings();
+
+// const rows = [
+//     {
+//         name: 'G2 Esports',
+//         matchRecord: '4-0',
+//         gameRecord: '12-3',
+//         plusMinus: '+9'
+//     },
+//     {
+//         name: 'NRG Esports',
+//         matchRecord: '4-1',
+//         gameRecord: '14-7',
+//         plusMinus: '+7'
+//     },
+// ];
 
 export default class BottomNavBar extends React.Component {
     state = {
